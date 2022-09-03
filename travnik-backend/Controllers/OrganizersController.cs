2b02b91ef8e9db6783ae77c806744590c82a300e
Lejla1502 <lejlasp@hotmail.com>
2022-09-03 19:20:49 +0200

    added get methods to organizers API

---
+++ travnik-backend/Controllers/OrganizersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using travnik_backend.Models;
+
+namespace travnik_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizersController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public OrganizersController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //GET: api/Organizer
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Organizer>>> GetEvents()
+        {
+            if (_dbContext.Organizers == null)
+                return NotFound();
+
+            return await _dbContext.Organizers.ToListAsync();
+        }
+
+        //GET: api/Organizer/5
+        [HttpGet("{id}")]
+        [ActionName(nameof(Organizer))]
+        public async Task<ActionResult<Organizer>> GetOrganizer(int id)
+        {
+            if (_dbContext.Organizers == null)
+            {
+                return NotFound();
+            }
+
+            var e = await _dbContext.Organizers.FindAsync(id);
+
+            if (e == null)
+                return NotFound();
+
+            return e;
+        }
+
+
+    }
+}
