a68871294f00ff114b136c0bf40651195abd22a8
Lejla1502 <lejlasp@hotmail.com>
2022-09-04 18:04:50 +0200

    get methods added to accomodation API

--- travnik-backend/Controllers/AccomodationController.cs
+++ travnik-backend/Controllers/AccomodationController.cs
@@ -34,7 +34,10 @@ namespace travnik_backend.Controllers
             if (_dbContext.Accomodations == null)
                 return NotFound();
 
-            var a = await _dbContext.Accomodations.Include(x => x.AccomodationType).Where(y => y.Id == id).FirstAsync();
+            //even though there is accomodation with a given id, it still won't recognize it if it doen't have
+            //type id declared and it will return 404
+            var a = await _dbContext.Accomodations.Include(x => x.AccomodationType).Where(y => y.Id == id).FirstOrDefaultAsync();
+            //we nned to use firsOrDefualt so that it doesn't throw an error if it doesn't find an element
 
             if (a == null)
                 return NotFound();
