eada0d05f0153f554b25fb8ae947f888bd7f3411
Lejla1502 <=>
2022-09-20 15:35:33 +0200

    adding room type

---
+++ travnik-backend/Models/RoomType.cs
@@ -0,0 +1,11 @@
+﻿namespace travnik_backend.Models
+{
+    public class RoomType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public double Price { get; set; }
+        public bool IsReserved { get; set; }
+    }
+}
