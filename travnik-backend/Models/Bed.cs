e2feff2306ca24f060d0086ef0ef5f9335b7c35e
Lejla1502 <=>
2022-09-20 15:37:45 +0200

    bed model

---
+++ travnik-backend/Models/Bed.cs
@@ -0,0 +1,9 @@
+﻿namespace travnik_backend.Models
+{
+    public class Bed
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Qty { get; set; }
+    }
+}
