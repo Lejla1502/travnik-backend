14e86fc7eaf8f2241ae0f7eb4efaaa83fb2716f9
Lejla1502 <lejlasp@hotmail.com>
2022-09-30 19:24:05 +0200

    query to display bed name and number for specific room of accomodation

--- travnik-backend/Controllers/AccomodationsController.cs
+++ travnik-backend/Controllers/AccomodationsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using travnik_backend.Models;
 using travnik_backend.Models.Accomodation;
 
@@ -38,8 +39,9 @@ namespace travnik_backend.Controllers
 
             //even though there is accomodation with a given id, it still won't recognize it if it doen't have
             //type id declared and it will return 404
-            
-            var a = await _dbContext.Accomodations.Include(x => x.AccomodationType).Include(x=>x.AccomodationRoomNames).ThenInclude(x=>x.RoomName).Where(y => y.Id == id).FirstOrDefaultAsync();
+
+            var a = await _dbContext.Accomodations.Where(y => y.Id == id).Include(x => x.AccomodationType).Include(x => x.AccomodationRoomNames).ThenInclude(x => x.RoomName).ThenInclude(x=>x.AccomodationRoomNames).ThenInclude(x=>x.AccomodationRoomNameBeds).ThenInclude(x=>x.Bed).FirstOrDefaultAsync();
+           // var b=a.Include(x=>x.AccomodationRoomNameBeds).ThenInclude(x=>x.Bed).FirstOrDefaultAsync();
             
             //we nned to use firsOrDefualt so that it doesn't throw an error if it doesn't find an element
 
